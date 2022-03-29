b469b384d915b2c51980162972e4ba1ac9d6b76b
kuldeep kumar <kuldeepgoswami1129@gmail.com>
2022-03-29 18:59:37 +0530

    update

---
+++ admin/login.php
@@ -0,0 +1,75 @@
+<?php include('../config/constants.php'); ?>
+
+<html>
+    <head><title>Login - Food Order System</title>
+    <link rel="stylesheet" href="../css/admin.css">
+</head>
+<body>
+    
+<div class="login">
+    <h1 class="text-center">Login</h1>  <br><br>
+    <?php
+    
+    if(isset($_SESSION['login']))
+    {
+        echo $_SESSION['login'];
+        unset ($_SESSION['login']);
+    }
+
+
+
+    ?>
+    <br><br>
+
+    <form action="" method="POST"  class="text-center">
+        Username  <br>
+        <input type="text" name="username" placeholder="Enter Username">  <br><br>
+        Password:   <br>
+        <input type="password" name="password" placeholder="Enter Password">
+        <br><br>
+
+        <input type="submit" name="submit" value="Login" class="btn-primary">  <br>
+
+    </form>
+    <br>
+
+
+
+
+
+    <p class="text-center">Created by - <a href="www.kuldeepkumar.com">Kuldeep Kumar</a></p>
+</div>
+</body>
+    
+</html>
+
+
+<?php
+
+if(isset($_POST['submit']))
+{
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $sql ="SELECT * FROM tbl_admin WHERE username='$username' AND password ='$password'";
+
+    $res = mysqli_query($conn,$sql);
+
+    $count = mysqli_num_rows($res);
+
+    if($count==1)
+    {
+        $_SESSION['login'] =" <div class='success'>Login Successful.</div>";
+
+        header('location:'.SITEURL.'admin/');
+    }
+    else
+    {
+        $_SESSION['login'] =" <div class='error'>Login Failed.</div>";
+
+        header('location:'.SITEURL.'admin/login.php');
+
+
+    }
+}
+?>
