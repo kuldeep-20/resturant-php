cd46d0cc0d6d077aac8605d8ad6091fb91d94567
kuldeep kumar <kuldeepgoswami1129@gmail.com>
2022-04-05 20:38:20 +0530

    status

--- admin/add-category.php
+++ admin/add-category.php
@@ -10,16 +10,29 @@
               echo $_SESSION['add'];
               unset($_SESSION['add']);
           }
+
+          if(isset($_SESSION['upload'])) 
+          {
+              echo $_SESSION['upload'];
+              unset($_SESSION['upload']);
+          }
           ?>
           <br><br>
 
          <!---Add category from start -->
-         <form action="" method="POST">
+         <form action="" method="POST" enctype="multipart/form-data">
              <table class="tbl-30">
                  <tr>
                      <td>Title</td>
                      <td>
                          <input type="text" name="title" placeholder="Category Title">
+                     </td>
+                 </tr>
+
+                 <tr>
+                     <td>Select Image </td>
+                     <td>
+                         <input type="file" name="image">
                      </td>
                  </tr>
 
@@ -68,19 +81,55 @@
                  $active = "No";
              }
 
+            // print_r($_FILES['image']);
+            // die();
+
+            if(isset($_FILES['image']['name']))
+            {
+                $image_name = $_FILES['image']['name'];
+
+                $ext = end(exploder('.',$image_name));
+
+                $image_name = "Food_Category_".rand(000,999).'.'.$ext;
+
+
+
+
+                $source_path = $_FILES['image']['tmp_name'];
+                $destination_path = "../images/category/".$image_name;
+
+                $upload = move_uploaded_file($source_path, $destination_path);
+
+                if($upload==false)
+                {
+                    $_SESSION['upload'] = "<div class='error'>Failed to Upload Image.</div>";
+                    header('location:'.SITEURL.'admin/add-category.php');
+                    die();
+                }
+
+            }
+            else
+            {
+                $image_name="";
+            }
+
+
+
              $sql = "INSERT INTO tbl_category SET
              title='$title',
+             image_name='$image_name',
              featured='$featured',
-             active='$active'";
+             active='$active'
+             ";
 
-            
+            $res = mysqli_query($conn , $sql);
 
-            $res = mysqli_query($conn, $sql) ;
+   
               
              if($res==true)
              {
                  $_SESSION['add'] = "<div class ='sucess'>Category Added Successfully.</div>";
-                 header('location'.SITEURL.'admin/manage-category.php');
+                 header('location :'.SITEURL.'admin/manage-category.php');
              }
              else{
                 $_SESSION['add'] = "<div class ='error'>Failed to Add.</div>";
--- admin/manage-category.php
+++ admin/manage-category.php
@@ -3,7 +3,7 @@
      <div class="wrapper">
          <h1>Manage Category</h1>
          <br><br>
-         
+
          <?php
          if(isset($_SESSION['add'])) 
           {
@@ -12,45 +12,89 @@
           }
           ?>
 
+          <br><br>
+
              <!--Button to add admin-->
              <a href="<?php echo SITEURL; ?>admin/add-category.php" class="btn-primary">Add Category</a>
              <br><br><br>
             <table class = "tbl-full">
                  <tr>
                      <th>S.N</th>
-                     <th>Full Name</th>
-                     <th>Username</th>
+                     <th>Title</th>
+                     <th>Image</th>
+                     <th>Featured</th>
+                     <th>Active</th>
                      <th>Actions</th>
                  </tr>
-                 <tr>
-                     <td>1.</td>
-                     <td>Kuldeep</td>
-                     <td>kuldeepkumar</td>
-                     <td>
-                     <a href="#" class ="btn-secondary">Update Admin</a>
-                        <a href="#" class ="btn-danger">Delete Admin</a>
-                     </td>
-                 </tr>
+                 <?php
 
-                 <tr>
-                     <td>2.</td>
-                     <td>Kuldeep</td>
-                     <td>kuldeepkumar</td>
-                     <td>
-                     <a href="#" class ="btn-secondary">Update Admin</a>
-                        <a href="#" class ="btn-danger">Delete Admin</a>
-                     </td>
-                 </tr>
+                 $sql = "SELECT * FROM tbl_category";
 
-                 <tr>
-                     <td>3.</td>
-                     <td>Kuldeep</td>
-                     <td>kuldeepkumar</td>
+                 $res = mysqli_query($conn, $sql);
+
+                 $count = mysqli_num_rows($res);
+
+                 $sn=1;
+
+                 if($count>0)
+                 {
+
+                    while($row=mysqli_fetch_assoc($res))
+                    {
+                        $id = $row['id'];
+                        $title = $row['title'];
+                        $image_name = $row['image_name'];
+                        $featured = $row['featured'];
+                        $active = $row['active'];
+                        ?>
+
+                <tr>
+                     <td><?php echo $sn++; ?></td>
+                     <td><?php echo $title; ?></td>
+
                      <td>
-                        <a href="#" class ="btn-secondary">Update Admin</a>
-                        <a href="#" class ="btn-danger">Delete Admin</a>
+                         <?php 
+                         if($image_name!="")
+                         {
+                             ?>
+                             <img src="<?php echo SITEURL; ?>images/category/<?php echo $image_name; ?>" width ="100px" >
+                             <?php
+
+
+                         }
+                         else
+                         {
+                         echo "<div class ='error'>Image Not Found.</div>";
+                         }
+                          ?>
+                    </td>
+
+                     <td><?php echo $featured; ?></td>
+                     <td><?php echo $active; ?></td>
+                     <td>
+                     <a href="#" class ="btn-secondary">Update Category</a>
+                        <a href="#" class ="btn-danger">Delete Category</a>
                      </td>
-                 </tr>
+                </tr>
+                <?php
+
+                    }
+
+                 }
+                 else{
+                     ?>
+
+                     <tr>
+                         <td colspan="6"><div class="error">No Category Added.</div></td>
+                     </tr>
+                     <?php
+
+                 }
+
+
+
+                 ?>
+
              </table>
 
      </div>
